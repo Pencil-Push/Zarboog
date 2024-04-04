97ae6950a7b56db2c403201bce55e81824f16c57
Pencil-Push <KnX72aL@gmail.com>
2024-04-04 17:56:05 -0400

    uhhh

--- Assets/Scripts/Spardu Scripts/Enemy_Controller.cs
+++ Assets/Scripts/Spardu Scripts/Enemy_Controller.cs
@@ -6,14 +6,15 @@ public class Enemy_Controller : MonoBehaviour
 {
     [Header ("Spawn Parameters")]
     //private float waitTimeBeforeSpawn = 5f;
-    private float spawnWaitTime;
+    private bool insideSpawn = false;
     private GameObject spawnPoint;
     public GameObject SpawnPoint01;
-    public GameObject SpawnPoint02;
-    public GameObject SpawnPoint03;
-    public GameObject SpawnPoint04;
-    public GameObject SpawnPoint05;
-    public GameObject SpawnPoint06;
+    //public GameObject SpawnPoint02;
+    //public GameObject SpawnPoint03;
+    //public GameObject SpawnPoint04;
+    //public GameObject SpawnPoint05;
+    //public GameObject SpawnPoint06;
+    public int currPoint;
 
     [Header ("Ship Prefabs")]
     [SerializeField] private GameObject Ship;
@@ -27,6 +28,13 @@ public class Enemy_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //GameObject Ship = Instantiate(prefab, new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y, 0), Quaternion.identity);
+
         
     }
 }
