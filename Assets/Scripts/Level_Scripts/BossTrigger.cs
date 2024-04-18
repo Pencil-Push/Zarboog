cf5500773e92944f6edfec8591980939e3df6809
Pencil-Push <KnX72aL@gmail.com>
2024-04-18 15:58:57 -0400

    boss_room

---
+++ Assets/Scripts/Level_Scripts/BossTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+    public Transform player;
+    public GameObject bossEntrance;
+    /*
+    private void Start()
+    {
+        GetComponent<BossRun>().enabled = false;
+    }
+    
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider col)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+
+        }
+    }
+    */
+}
