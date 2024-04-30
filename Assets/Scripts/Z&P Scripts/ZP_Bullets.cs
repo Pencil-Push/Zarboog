013ef2bf72325cd0cef362467aaac8cd45af3b9f
Pencil-Push <KnX72aL@gmail.com>
2024-04-30 15:58:08 -0400

    shot_anim

--- Assets/Scripts/Z&P Scripts/ZP_Bullets.cs
+++ Assets/Scripts/Z&P Scripts/ZP_Bullets.cs
@@ -8,26 +8,34 @@ public class ZP_Bullets : MonoBehaviour
     [SerializeField] private float damage;
 
     [SerializeField] private Rigidbody2D rb;
+    private Animator shAnim;
     
     // Start is called before the first frame update
+    void Start()
+    {
+        shAnim = GetComponent<Animator>();
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Enemy")
         {
             collision.GetComponent<EnemyHealth>().TakeDamage(damage);
+            //shAnim.SetTrigger("Explode");
             Destroy(gameObject);
         }
 
         if(collision.tag == "Ship")
         {
             collision.GetComponent<Ship_Health>().TakeDamage(damage);
+            //shAnim.SetTrigger("Explode");
             Destroy(gameObject);
         }
 
         if(collision.tag == "Boss")
         {
             collision.GetComponent<Boss_Health>().TakeDamage(damage);
+            //shAnim.SetTrigger("Explode");
             Destroy(gameObject);
         }
     }
