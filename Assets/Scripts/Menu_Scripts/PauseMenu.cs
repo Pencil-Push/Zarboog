8fbd1784d25b7b2452a22fe6157488c50e8a8bba
Pencil-Push <KnX72aL@gmail.com>
2024-05-26 11:28:04 -0400

    Updated a few things

--- Assets/Scripts/Menu_Scripts/PauseMenu.cs
+++ Assets/Scripts/Menu_Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -35,5 +36,10 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
     }
 }
